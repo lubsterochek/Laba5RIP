8c305ee1196427dc1cce0b2942c674677b9f2b00
Yura <lubsterplay0703@gmail.com>
2025-03-02 00:08:20 +0300

    git commit

--- src/main/java/ru/itmo/yurchik/csvReaderWriter/CsvReader.java
+++ src/main/java/ru/itmo/yurchik/csvReaderWriter/CsvReader.java
@@ -2,8 +2,12 @@ package ru.itmo.yurchik.csvReaderWriter;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import ru.itmo.yurchik.collection.DragonCollection;
+import ru.itmo.yurchik.model.*;
 
 import java.io.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CsvReader {
@@ -33,6 +37,47 @@ public class CsvReader {
             data.add(nextLine);
         }
     }
+
+    //Парсим дракона из файла
+    private Dragon parseDragon(String line) {
+        try {
+            String[] values = line.replace("\"", "").split(",");
+            String name = values[1];
+            float x = Float.parseFloat(values[2]);
+            float y = Float.parseFloat(values[3]);
+            ZonedDateTime date = ZonedDateTime.parse(values[4], DateTimeFormatter.ISO_ZONED_DATE_TIME);
+            Long age = Long.parseLong(values[5]);
+            Color color = Color.valueOf(values[6]);
+            DragonType type = DragonType.valueOf(values[7]);
+            DragonCharacter character = DragonCharacter.valueOf(values[8]);
+            float depth = Float.parseFloat(values[9]);
+            float numberOfTreasures = Float.parseFloat(values[10]);
+
+            return new Dragon(name, new Coordinates(x, y), date, age, color, type, character, new DragonCave(depth, numberOfTreasures));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка парсинга строки: " + line);
+            return null;
+        }
+    }
+
+    public void loadDragons(DragonCollection dragonCollection) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                Dragon dragon = parseDragon(line);
+                if (dragon != null) {
+                    dragonCollection.addDragon(dragon);
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Ошибка чтения файл: ");
+            System.exit(0);
+        }
+    }
+
+
+
 
     public ArrayList<String[]> getData() {
         return data;
