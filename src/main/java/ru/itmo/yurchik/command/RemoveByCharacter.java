556f39944653d66c6b8400915a8c288109cc1a67
ki40lf <shirokovlev2006@gmail.com>
2025-03-01 20:20:51 +0300

    git commit

---
+++ src/main/java/ru/itmo/yurchik/command/RemoveByCharacter.java
@@ -0,0 +1,49 @@
+package ru.itmo.yurchik.command;
+
+import ru.itmo.yurchik.collection.DragonCollection;
+import ru.itmo.yurchik.command.base.Command;
+import ru.itmo.yurchik.command.base.Environment;
+import ru.itmo.yurchik.command.exception.CommandException;
+import ru.itmo.yurchik.model.DragonCharacter;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class RemoveByCharacter extends Command {;
+    private final DragonCollection collection;
+    public RemoveByCharacter(DragonCollection collection) {
+        super("remove_all_by_character");
+        this.collection = collection;
+    }
+
+    @Override
+    public void execute(Environment env, InputStream stdin, PrintStream stdout) throws CommandException {
+        Scanner scanner = new Scanner(stdin);
+
+        DragonCharacter finalCharacter;
+        while (true) {
+            try {
+                stdout.print("Введите характер дракона (CHAOTIC, CHAOTIC_EVIL, FICKLE): ");
+                finalCharacter = DragonCharacter.valueOf(scanner.next().toUpperCase()); // Преобразуем ввод в ENUM
+                break;
+            } catch (IllegalArgumentException e) {
+                stdout.println("Ошибка: Неверный ввод. Попробуйте снова.");
+            }
+        }
+
+        DragonCharacter finalCharacter1 = finalCharacter;
+        boolean removed = collection.getDragons().removeIf(dragon -> dragon.getCharacter() == finalCharacter1);
+
+        if (removed) {
+            stdout.println("Драконы с характером " + finalCharacter + " успешно удалены.");
+        } else {
+            throw new CommandException("Драконов с характером " + finalCharacter + " не найдено.");
+        }
+    }
+
+    @Override
+    public String getHelp() {
+        return "remove all by character";
+    }
+}
