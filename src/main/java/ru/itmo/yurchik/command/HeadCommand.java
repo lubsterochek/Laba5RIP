ef593151d47ae213bf0225a2fe09951856929d40
ki40lf <shirokovlev2006@gmail.com>
2025-03-06 14:51:58 +0300

    git commit

--- src/main/java/ru/itmo/yurchik/command/HeadCommand.java
+++ src/main/java/ru/itmo/yurchik/command/HeadCommand.java
@@ -36,7 +36,7 @@ public class HeadCommand extends Command {
         if (dragons.getDragons().isEmpty()) {
             throw new CommandException("Коллекция пуста! Попробуйте другую команду");
         }
-        System.out.printf("Первый элемент коллекции: %1$s", dragons.getDragons().getFirst());
+        System.out.println("Первый элемент коллекции: %1$s" + dragons.getDragons().getFirst());
     }
     /**
      * Метод для получения описания команды
